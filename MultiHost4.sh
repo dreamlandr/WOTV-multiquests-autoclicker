89569617c65b129b61009bd7e01009527c46b21b
dreamlandr <tiago.amano@gmail.com>
2023-06-11 08:22:50 -0300

    Adding Multi host 4 and Raid Auto

---
+++ MultiHost4.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+trap "exit 0" 0 1 2 3 9 15 ERR
+
+case $1 in
+    cel)
+        source $(dirname $BASH_SOURCE)/varcel
+        ;;
+    emu)
+        source $(dirname $BASH_SOURCE)/variables
+        ;;
+    *)
+        echo Not a valid platform;
+        exit 0
+        ;;
+esac
+
+while true; do
+    adb shell "
+        input tap $embarkX $embarkY;
+        input tap $applyX $applyY;
+    "
+done;
---
+++ RaidAuto.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+trap "exit 0" 0 1 2 3 9 15 ERR
+
+case $1 in
+    cel)
+        source $(dirname $BASH_SOURCE)/varcel
+        ;;
+    emu)
+        source $(dirname $BASH_SOURCE)/variables
+        ;;
+    *)
+        echo Not a valid platform;
+        exit 0
+        ;;
+esac
+
+while true; do 
+    adb shell "
+        input tap $confirmX $confirmY;
+        input tap $embarkX $embarkY;
+        input tap $applyX $applyY;
+        input tap $reEmbarkX $reEmbarkY;
+    "
+done 
